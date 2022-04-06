135011800b3df810dec31c85cce74683ea4c6933
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-04-06 16:42:49 +0300

    Место реакт 10.7

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../images/Vector.svg'
+
+function Header() {
+    return (
+        <header className="header">
+        <img src={logo} className="header__logo" alt="Логотип"/>
+    </header>
+    );
+  }
+  
+  export default Header;
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import api from './utils.js/Api'
+import Card from './Card'
+function Main (props) {
+    const [cards, setCards] = React.useState([])
+    const [userInfo, setUserInfo] = React.useState({})
+  React.useEffect(() => {
+    Promise.all([api.getProfile(), api.getInitialCards()])
+    .then(([profileInfo, card] ) => {
+        setUserInfo(profileInfo)
+        setCards(card)
+      }).catch((err) => {
+        console.error(err);
+      })
+    }, [])
+    
+    return (
+        <main className="main-content">
+            <section className="profile">
+                <div className="profile__container">
+                    <img src={userInfo.avatar} className="profile__avatar" alt="Аватар"/>
+                    <button type="button" className="profile__pen" onClick={props.onEditAvatar}></button>
+                    <div className="profile__info">
+                        <div className="profile__content">
+                            <h1 className="profile__name">{userInfo.name}</h1>
+                            <button type="button" className="profile__button" onClick={props.onEditProfile}></button>
+                        </div>
+                        <p className="profile__job">{userInfo.about}</p>
+                    </div>
+                </div>
+                <button type="button" className="profile__button-plus" onClick={props.onAddPlace}></button>
+                
+            </section>
+            <section className="card">
+            {cards.map((card) => {
+                return (
+          <Card
+            key={card._id}
+            card={card}
+            onCardClick={props.onCardClick}
+          />
+                )
+            })}
+            </section>
+            
+        </main>
+    )
+}
+export default Main
